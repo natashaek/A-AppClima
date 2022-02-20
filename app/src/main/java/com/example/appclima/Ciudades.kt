7e0c014d84ef94f1c012269e81e3fc3593fd20fb
nattashaek <natasha.ek@cbtis72.edu.mx>
2022-02-20 14:31:38 -0500

    Añadiendo un intent

--- app/src/main/java/com/example/appclima/Ciudades.kt
+++ app/src/main/java/com/example/appclima/Ciudades.kt
@@ -1,11 +1,27 @@
 package com.example.appclima
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class Ciudades : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ciudades)
+
+        val bFcp = findViewById<Button>(R.id.bFcp)
+        val bChetu = findViewById<Button>(R.id.bChetu)
+
+        bFcp.setOnClickListener(View.OnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        })
+
+        bChetu.setOnClickListener(View.OnClickListener {
+            Toast.makeText(this, "Chetumal", Toast.LENGTH_SHORT).show()
+        })
     }
 }
